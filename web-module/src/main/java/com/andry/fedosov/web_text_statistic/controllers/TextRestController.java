9a8f74b16737551616b1d5b4d9a0b2bd3df541c8
Andrii Fedosov <shinkun1993@gmail.com>
2018-06-20 14:00:29 +0300

    update cross origin annotation value and request mapping value

--- web-module/src/main/java/com/andry/fedosov/web_text_statistic/controllers/TextRestController.java
+++ web-module/src/main/java/com/andry/fedosov/web_text_statistic/controllers/TextRestController.java
@@ -22,8 +22,8 @@ import java.io.IOException;
 import java.net.URI;
 
 @RestController
-@CrossOrigin(origins = "https://text-statistic-ui.herokuapp.com")
-@RequestMapping(value = "/")
+@CrossOrigin(origins = "https://localhost:3000")
+@RequestMapping(value = "/v1/statistics")
 
 public class TextRestController {
 
